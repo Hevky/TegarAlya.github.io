2535b68a809a7c97c37852add97643fc9f10e711
Hevky <88450725+Hevky@users.noreply.github.com>
2023-02-09 21:05:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,223 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+<html lang="id">
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>HALLO AYYY</title>
+</head>
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
+<body>
+  <!-- Header -->
+  <audio control autoplay loop>
+    <source src = "./music/Andra And The Backbone - Sempurna (Official Music Video).mp3" type="audio/mp3"/>
+  </audio>
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#Alga">
+            <h1><span>03-</span>02-<span>20</span>23</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#Alga" data-after="Home">Home</a></li>
+            <li><a href="#how" data-after="how">About Alga</a></li>
+            <li><a href="#Gallery" data-after="Gallery">Gallery</a></li>
+            <li><a href="#about" data-after="about">Sifat Awe</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
 
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
+
+  <!-- Hero Section  -->
+  <section id="Alga">
+    <div class="Alga container">
+      <div>
+        <h1>Welcome <span></span></h1>
+        <h1>In Our Website <span></span></h1>
+        <h1>Tegar And Alya <span></span></h1>
+        <a href="#Gallery" type="button" class="cta">Gallery</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+
+  <div id="Alya">
+  <section id="how">
+    <div class="how container">
+      <div class="col-left">
+        <div class="how-img">
+          <img src="./img/img-4.jpg" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>Alya</span></h1>
+        <h2>Alya Widyadari Cahyaningtyas </h2>
+        <h2>Bekasi, 7 Maret 2004 
+          <h2>Cewenya Tegar
+          </p>
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <div id="Tegar">
+  <section id="how">
+    <div class="how container">
+      <div class="col-left">
+        <div class="how-img">
+          <img src="./img/img-2.jpg" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>Tegar</span></h1>
+        <h2>Hevky Tegar Hirmanda </h2>
+        <h2>Bekasi. 26 juli 2004
+          <h2>Cowonya Awe
+          </p>
+        </p>
+      </div>
+    </div>
+  </section>
+    <!-- End Service Section -->
+
+  <!-- Projects Section -->
+  <section id="Gallery">
+    <div class="Gallery container">
+      <div class="Gallery-header">
+        <h1 class="section-title">Gallery <span>ALGA</span></h1>
+      </div>
+      <div class="all-Gallery">
+        <div class="Gallery-item">
+          <div class="Gallery-info">
+            <h1>1st Date</h1>
+            <h2>date pertama kita ke bca</h2>
+            <p>Menurut ku date pertama kita di bca, dengan kondisi yang sangat amat aneh, yang dimana kita lagi ada pelajaran
+              trus berdua minta ijin AHAHAHA, mana pas lgi otw bcanya ujan neduh beberapa jam tetep ga berenti trus jadinya ujan2an. pas udah sampe
+              bcanya malah gabisa ujung2nya makan mie bareng 
+            </p>
+          </div>
+          <div class="Gallery-img">
+            <img src="./img/img-1.png.jpg" alt="img">
+          </div>
+        </div>
+        <div class="Gallery-item">
+          <div class="Gallery-info">
+            <h1>Project 2</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="Gallery-img">
+            <img src="./img/img-1.png.jpg" alt="img">
+          </div>
+        </div>
+        <div class="Gallery-item">
+          <div class="Gallery-info">
+            <h1>Project 3</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="Gallery-img">
+            <img src="./img/img-1.png.jpg" alt="img">
+          </div>
+        </div>
+        <div class="Gallery-item">
+          <div class="Gallery-info">
+            <h1>Project 4</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="Gallery-img">
+            <img src="./img/img-1.png.jpg" alt="img">
+          </div>
+        </div>
+        <div class="Gallery-item">
+          <div class="Gallery-info">
+            <h1>Project 5</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="Gallery-img">
+            <img src="./img/img-1.png.jpg" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="about-top">
+        <h1 class="section-title">HALLO<span> </span>AYYY</h1>
+        <p>Hallo Ayyy mungkin gabisa banyak, yang aku tunjukin cuman beberapa aja jadi have fun!</p>
+      </div>
+      <div class="about-bottom">
+        <div class="about-item">
+          <div class="icon"><img src="./img/img-1.png.jpg" alt="img">/></div>
+          <h2>Awe Marah</h2>
+          <p>Alya Widyadari cahyaningtyas, Kalo marah kek orang gadikenal tapi gwej dengan senang hati dimarahin ama Awe
+            Asal MARAHNYA JELASSSS 🤤</p>
+        </div>
+        <div class="about-item">
+          <div class="icon"><img src="./img/img-1.png.jpg" alt="img" /></div>
+          <h2>AWE NANGIS</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="about-item">
+          <div class="icon"><img src="./img/img-1.png.jpg" alt="img" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="about-item">
+          <div class="icon"><img src="./img/img-1.png.jpg" alt="img" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>T</span>egar <span>A</span>lya</h1>
+      </div>
+      <h2>Website bucin dari tegar</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+
+
+
+</html>
